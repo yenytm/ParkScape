5248a07ebb6f70a429e938b08b27eb532e1dda55
navia <naviamatute@yahoo.com>
2023-11-20 20:59:22 -0500

    nav and footer

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,16 +1,23 @@
 import {Link, Outlet } from "react-router-dom";
 import "./Root.css";
+import { About } from "../pages/About";
 
 export default function Root() {
   return (
     <>
-      <nav style={{display: "flex", margin: "auto", backgroundColor: "Blue"}}>
+      <nav style={{ display: "flex", margin: "auto", backgroundColor: "" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Outlet />
+
+      <footer
+        style={{ display: "flex", backgroundColor: "", marginTop: "3rem" }}
+      >
+        <span> © 2023 ParkScape Co.</span>
         <Link to="/">Home</Link>
         <Link to="/">About</Link>
-      </nav>
-      <Outlet />
-      <footer style={{display: "flex", backgroundColor: "orange", margin: "auto"}}>
-      <p>Footer</p>
       </footer>
     </>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+
+
+export function About () {
+return (
+    <>
+    <h1> ABOUT </h1>
+    </>
+)
+
+}
