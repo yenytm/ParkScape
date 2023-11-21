e89ce4f770675dde179d723852ef099d0d23dab3
navia <naviamatute@yahoo.com>
2023-11-20 20:15:17 -0500

    orange footer

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -9,7 +9,7 @@ export default function Root() {
         
       </nav>
       <Outlet />
-      <footer style={{display: "flex", backgroundColor: "green", margin: "auto"}}>
+      <footer style={{display: "flex", backgroundColor: "orange", margin: "auto"}}>
       <p>Footer</p>
       </footer>
     </>
