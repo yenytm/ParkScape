89a3c638374ad294ed484b4bff1768f7f37ddf81
navia <naviamatute@yahoo.com>
2023-11-20 20:32:16 -0500

    getting ready

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -6,7 +6,7 @@ export default function Root() {
     <>
       <nav style={{display: "flex", margin: "auto", backgroundColor: "Blue"}}>
         <Link to="/">Home</Link>
-        
+        <Link to="/">About</Link>
       </nav>
       <Outlet />
       <footer style={{display: "flex", backgroundColor: "orange", margin: "auto"}}>
