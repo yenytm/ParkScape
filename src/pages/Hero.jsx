a2b0206bc085af21ceb03533d943e8348f068b2c
yenytm <143008413+yenytm@users.noreply.github.com>
2023-11-22 00:45:26 -0500

    heroMain Body

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,20 @@
+import './hero.css'
+
+export  function Hero () {
+    return(
+
+
+        <div className='heroBody'>
+        <div className="mainBody">
+            <a target='_blank' href="">
+                <img className='heroMainPic' src="src/assets/ParkScape.png" alt="" />
+                {/* <img className='heroMainPic' src="src/assets/mainPic.png" alt="" /> */}
+
+
+            </a>
+        </div>
+
+
+    </div>
+    )
+}
