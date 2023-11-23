ff8be0437a7e01f9dda0acf2c16b0836af93e7b9
navia <naviamatute@yahoo.com>
2023-11-22 20:18:05 -0500

    Adding addresses

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 export function Navbar() {
   return (
     <>
-      <nav style={{ display: "flex", margin: "auto", backgroundColor: "" }}>
+      <nav
+        className="topNav"
+        //  style={{ display: "flex", margin: "auto", backgroundColor: "" }}
+      >
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </nav>
