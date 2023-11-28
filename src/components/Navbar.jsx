dc0d08eede324eefd4b8e32ab3fa16748dc3e0ce
navia <naviamatute@yahoo.com>
2023-11-27 19:06:21 -0500

    fixing nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,17 @@ import { Link } from "react-router-dom";
 export function Navbar() {
   return (
     <>
-      <nav
-        className="topNav"
-        //  style={{ display: "flex", margin: "auto", backgroundColor: "" }}
-      >
-
+      <nav className="topNav">
+        <div style={{ flex: 1 }}>
+          <img
+            className="LogoPic"
+            src="src/assets/ParkScapeTopNav.png"
+            alt=""
+          />
+        </div>
         <Link to="/">Home</Link>
-        <img className='LogoPic' src="src/assets/Logo.png" alt=""/>
         <Link to="/about">About</Link>
-
       </nav>
-
-      
     </>
   );
 }
