2067ae66821b0814f63986e74829223899416e63
yenytm <143008413+yenytm@users.noreply.github.com>
2023-12-02 11:44:11 -0500

    parkDetails-layout

--- src/pages/ParkDetails.jsx
+++ src/pages/ParkDetails.jsx
@@ -4,9 +4,9 @@ import { transformParkData } from "./Parks";
 const api_key = import.meta.env.VITE_NPS_KEY;
 const url = `https://developer.nps.gov/api/v1/parks?API_KEY=${api_key}`;
 
-export const getParkDetails = async ({params}) => {
+export const getParkDetails = async ({ params }) => {
   try {
-    const response = await fetch(url+`&parkCode=${params.code}`);
+    const response = await fetch(url + `&parkCode=${params.code}`);
     const data = await response.json();
     const cleanData = data.data.map(transformParkData);
     console.log(cleanData);
@@ -17,12 +17,42 @@ export const getParkDetails = async ({params}) => {
 };
 
 export default function ParkDetails() {
-    const park = useLoaderData()
+  const park = useLoaderData();
   return (
     <div>
-      <div id="heroBody" className="flex-auto place-items-center">
-        <span>{park.description}</span>
-        <img src="arches.png" alt="" />
+      <div className="carousel rounded-box ">
+        <div className="carousel-item w-50 h-[24rem]">
+          <img src={park.images[0].url} alt={park.images[0].altText} />
+        </div>
+        <div className="carousel-item w-50 h-[24rem]">
+          <img src={park.images[1].url} alt={park.images[1].altText} />
+        </div>
+        <div className="carousel-item w-50 h-[24rem]">
+          <img src={park.images[2].url} alt={park.images[2].altText} />
+        </div>
+      </div>
+      <div>
+        <h1 className="card-title">{park.fullName}</h1>
+        <p>
+          Location: {}
+          <span>{park.addresses[0].line1}, </span>
+          <span>
+            {" "}
+            {park.addresses[0].city} {}
+            {park.addresses[0].stateCode}, {}
+            {park.addresses[0].postalCode}
+          </span>
+        </p>
+        <p>
+          Operating Hours: {}
+          <span>{park.operatingHours[0].name}, </span>
+        </p>
+
+
+        <p>
+          Park Description: {} {park.description}
+        </p>
+        
       </div>
     </div>
   );
--- src/pages/Parks.jsx
+++ src/pages/Parks.jsx
@@ -7,127 +7,127 @@ const api_key = import.meta.env.VITE_NPS_KEY;
 const url = `https://developer.nps.gov/api/v1/parks?API_KEY=${api_key}`;
 
 export function transformParkData(data) {
-	const {
-		id,
-		url,
-		fullName,
-		parkCode,
-		description,
-		latitude,
-		longitude,
-		activities,
-		topics,
-		states,
-		contacts,
-		directionsInfo,
-		operatingHours,
-		addresses,
-		images,
-		name,
-		designation,
-	} = data;
+  const {
+    id,
+    url,
+    fullName,
+    parkCode,
+    description,
+    latitude,
+    longitude,
+    activities,
+    topics,
+    states,
+    contacts,
+    directionsInfo,
+    operatingHours,
+    addresses,
+    images,
+    name,
+    designation,
+  } = data;
 
-	return {
-		id,
-		url,
-		fullName,
-		parkCode,
-		description,
-		latitude,
-		longitude,
-		activities,
-		topics,
-		state: states,
-		phoneNumbers: contacts.phoneNumbers.map((phone) => phone.phoneNumber),
-		directionsInfo,
-		operatingHours: operatingHours.map((hours) => ({
-			name: hours.name,
-			exceptions: hours.exceptions.map((exception) => ({
-				name: exception.name,
-				startDate: exception.startDate,
-				endDate: exception.endDate,
-				hours: exception.exceptionHours,
-			})),
-			standardHours: hours.standardHours,
-		})),
-		addresses: addresses.map((address) => ({
-			postalCode: address.postalCode,
-			city: address.city,
-			stateCode: address.stateCode,
-			countryCode: address.countryCode,
-			provinceTerritoryCode: address.provinceTerritoryCode,
-			line1: address.line1,
-			line2: address.line2,
-			line3: address.line3,
-			type: address.type,
-		})),
-		images: images.map((image) => ({
-			credit: image.credit,
-			title: image.title,
-			altText: image.altText,
-			caption: image.caption,
-			url: image.url,
-		})),
-		name,
-	};
+  return {
+    id,
+    url,
+    fullName,
+    parkCode,
+    description,
+    latitude,
+    longitude,
+    activities,
+    topics,
+    state: states,
+    phoneNumbers: contacts.phoneNumbers.map((phone) => phone.phoneNumber),
+    directionsInfo,
+    operatingHours: operatingHours.map((hours) => ({
+      name: hours.name,
+      exceptions: hours.exceptions.map((exception) => ({
+        name: exception.name,
+        startDate: exception.startDate,
+        endDate: exception.endDate,
+        hours: exception.exceptionHours,
+      })),
+      standardHours: hours.standardHours,
+    })),
+    addresses: addresses.map((address) => ({
+      postalCode: address.postalCode,
+      city: address.city,
+      stateCode: address.stateCode,
+      countryCode: address.countryCode,
+      provinceTerritoryCode: address.provinceTerritoryCode,
+      line1: address.line1,
+      line2: address.line2,
+      line3: address.line3,
+      type: address.type,
+    })),
+    images: images.map((image) => ({
+      credit: image.credit,
+      title: image.title,
+      altText: image.altText,
+      caption: image.caption,
+      url: image.url,
+    })),
+    name,
+  };
 }
 
 export async function getParks() {
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-		const cleanData = data.data.map(transformParkData);
-		console.log(cleanData);
-		return cleanData;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const cleanData = data.data.map(transformParkData);
+    console.log(cleanData);
+    return cleanData;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default function Parks() {
-	const parks = useLoaderData();
-	const [filteredParks, setFilteredParks] = useState(parks);
-	const [activitiesIds, setActivitiesIds] = useState([]);
+  const parks = useLoaderData();
+  const [filteredParks, setFilteredParks] = useState(parks);
+  const [activitiesIds, setActivitiesIds] = useState([]);
 
-	useEffect(() => {
-		if (activitiesIds.length === 0) {
-			setFilteredParks(parks);
-			return;
-		}
+  useEffect(() => {
+    if (activitiesIds.length === 0) {
+      setFilteredParks(parks);
+      return;
+    }
 
-		const filterParks = async () => {
-			const data = await getParksByActivities(activitiesIds);
-			setFilteredParks(data);
-		};
-		filterParks();
-	}, [activitiesIds, parks]);
+    const filterParks = async () => {
+      const data = await getParksByActivities(activitiesIds);
+      setFilteredParks(data);
+    };
+    filterParks();
+  }, [activitiesIds, parks]);
 
-	return (
-		<>
-			<ul className="flex gap-4 flex-wrap  justify-center">
-				{filteredParks.length > 0 ? (
-					filteredParks.map((park) => (
-						<li key={park.id}>
-							<div className="card w-96 h-[30rem]  bg-base-100 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-120 duration-300 	">
-								<div className="h-[15rem] w-96 rounded-md overflow-hidden relative">
-									<img
-										className="h-full w-full object-cover"
-										src={park.images[0].url}
-										alt={park.images[0].altText}
-									/>
-								</div>
+  return (
+    <>
+      <ul className="flex gap-4 flex-wrap  justify-center">
+        {filteredParks.length > 0 ? (
+          filteredParks.map((park) => (
+            <li key={park.id}>
+              <div className="card w-96 h-[30rem]  bg-base-100 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-120 duration-300 	">
+                <div className="h-[15rem] w-96 rounded-md overflow-hidden relative">
+                  <img
+                    className="h-full w-full object-cover"
+                    src={park.images[0].url}
+                    alt={park.images[0].altText}
+                  />
+                </div>
 
-								<div className="card-body h-54 ">
-									<h2 className="card-title">
-										{park.fullName}
-									</h2>
-									<span>
-									{park.addresses[0].line1}, </span>
-										
-									<span>	{park.addresses[0].city} { }
-									{park.addresses[0].stateCode}, { }
-									{park.addresses[0].postalCode}</span>
-									{/*
+                <div className="card-body h-54 ">
+                  <h2 className="card-title">{park.fullName}</h2>
+                  <span>{park.addresses[0].line1}, </span>
+
+                  <span>
+                    {" "}
+                    {park.addresses[0].city} {}
+                    {park.addresses[0].stateCode}, {}
+                    {park.addresses[0].postalCode}
+                  </span>
+                  {/*
 									<ul className="flex flex-col gap-2">
                   {park.activities.map((activity) => (
 											<li key={activity.id}>
@@ -145,22 +145,22 @@ export default function Parks() {
                         ))}
                         </ul>
                       */}
-								</div>
-								<div className="card-actions justify-center pb-4">
-										<Link
-											to={`/park/${park.name}/${park.parkCode}`}
-											className="btn bg-green-700 text-white hover:bg-white hover:text-green-700 hover:border-green-700">
-											More Info
-										</Link>
-									</div>
-
-							</div>
-						</li>
-					))
-				) : (
-					<h1>No Parks Found</h1>
-				)}
-			</ul>
-		</>
-	);
+                </div>
+                <div className="card-actions justify-center pb-4">
+                  <Link
+                    to={`/park/${park.name}/${park.parkCode}`}
+                    className="btn bg-green-700 text-white hover:bg-white hover:text-green-700 hover:border-green-700"
+                  >
+                    More Info
+                  </Link>
+                </div>
+              </div>
+            </li>
+          ))
+        ) : (
+          <h1>No Parks Found</h1>
+        )}
+      </ul>
+    </>
+  );
 }
