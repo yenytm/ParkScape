ea2dc6e1d949a233e24783b3f2ccd4da0d7aaec0
yenytm <143008413+yenytm@users.noreply.github.com>
2023-12-15 00:42:33 -0500

    userPro

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 export default function UserProfile() {
   return (
@@ -8,14 +8,15 @@ export default function UserProfile() {
 <>
 
 <Navbar />
-<main className='pb-10'>
+<main className='pb-10 '>
 <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="mb-5 text-5xl font-bold">Under Construction!</h1>
+      <button className="btn border-orange-700 border-4 hover:bg-orange-400 hover:text-white">
+      <Link to="/parks">In the meantime, click here to explore some parks!</Link>
+        </button>
     </div>
   </div>
 </div>
